5b21845bd349bce6e5b0ed1230e08e9d2a1c6002
Maxim <55555lab@mail.ru>
2024-04-15 13:58:35 +0300

    KS-2-2 task complete

---
+++ src/main/kotlin/lesson_2/lesson2-task2.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_2
+
+fun main() {
+    /*
+    В компании работало 50 человек с зарплатой по 30000 рублей. Затем в компанию устроилось 30 стажеров,
+    которым назначили зарплату в 20000 рублей каждому. Пишем часть гипотетического софта для бухгалтерии,
+    который будет считать:
+дабавил расчеты:
+– Расходы на выплату зарплаты постоянных сотрудников;
+– Общие расходы по ЗП после прихода стажеров;
+– Среднюю ЗП одного сотрудника после устройства стажеров.
+
+Каждый пункт посчитать и сохранить в переменную с соответствующим названием. Все значения необходимо вывести в виде целых чисел.
+     */
+    val numberOfPermanentEmployees = 50
+    val salaryOfPermanentEmployees = 30000
+    val numberOfInterns = 30
+    val salaryOfInterns = 20000
+
+    val expsForSalaryPermanentEmployees: Int = numberOfPermanentEmployees * salaryOfPermanentEmployees
+    println("Расходы на выплату зарплаты постоянных сотрудников $expsForSalaryPermanentEmployees рублей")
+    val expsForSalaryInterns: Int = numberOfInterns * salaryOfInterns
+    println("Общие расходы по ЗП на стажеров $expsForSalaryInterns рублей")
+    val totalExpsForSalary: Int = expsForSalaryInterns + expsForSalaryPermanentEmployees
+    println("Общие расходы по ЗП после прихода стажеров $totalExpsForSalary рублей")
+    val totalAmountEmployees: Int = numberOfPermanentEmployees + numberOfInterns
+    println("Общее количество сотрудников $totalAmountEmployees человек")
+    val averageSalaryEmployees: Int = totalExpsForSalary / totalAmountEmployees
+    println("Средняя ЗП одного сотрудника после устройства стажеров $averageSalaryEmployees рублей")
+}
