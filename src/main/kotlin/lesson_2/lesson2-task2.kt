59b4b485e3eb708aab2dd5abcc29bb653ac4c9a8
Maxim <55555lab@mail.ru>
2024-04-15 14:12:14 +0300

    KS-2-2 fix names variability

--- src/main/kotlin/lesson_2/lesson2-task2.kt
+++ src/main/kotlin/lesson_2/lesson2-task2.kt
@@ -5,7 +5,7 @@ fun main() {
     В компании работало 50 человек с зарплатой по 30000 рублей. Затем в компанию устроилось 30 стажеров,
     которым назначили зарплату в 20000 рублей каждому. Пишем часть гипотетического софта для бухгалтерии,
     который будет считать:
-
+дабавил расчеты:
 – Расходы на выплату зарплаты постоянных сотрудников;
 – Общие расходы по ЗП после прихода стажеров;
 – Среднюю ЗП одного сотрудника после устройства стажеров.
@@ -14,17 +14,17 @@ fun main() {
      */
     val numberOfPermanentEmployees = 50
     val salaryOfPermanentEmployees = 30000
-    val numberOfTrainee = 30
-    val salaryOfTrainee = 20000
+    val numberOfInterns = 30
+    val salaryOfInterns = 20000
 
-    val expsForPayingPermanentEmployees: Int = numberOfPermanentEmployees * salaryOfPermanentEmployees
-    println("Расходы на выплату зарплаты постоянных сотрудников $expsForPayingPermanentEmployees рублей")
-    val expsForPayingTrainee: Int = numberOfTrainee * salaryOfTrainee
-    println("Общие расходы по ЗП на стажеров $expsForPayingTrainee рублей")
-    val generalExpsForPaying: Int = expsForPayingTrainee + expsForPayingPermanentEmployees
-    println("Общие расходы по ЗП после прихода стажеров $generalExpsForPaying рублей")
-    val generalAmountPersonal: Int = numberOfPermanentEmployees + numberOfTrainee
-    println("Общие количество сотрудников $generalAmountPersonal человек")
-    val avaragySalaryEmployees: Int = generalExpsForPaying / generalAmountPersonal
-    println("Среднюю ЗП одного сотрудника после устройства стажеров $avaragySalaryEmployees рублей")
+    val expsForSalaryPermanentEmployees: Int = numberOfPermanentEmployees * salaryOfPermanentEmployees
+    println("Расходы на выплату зарплаты постоянных сотрудников $expsForSalaryPermanentEmployees рублей")
+    val expsForSalaryInterns: Int = numberOfInterns * salaryOfInterns
+    println("Общие расходы по ЗП на стажеров $expsForSalaryInterns рублей")
+    val totalExpsForSalary: Int = expsForSalaryInterns + expsForSalaryPermanentEmployees
+    println("Общие расходы по ЗП после прихода стажеров $totalExpsForSalary рублей")
+    val totalAmountEmployees: Int = numberOfPermanentEmployees + numberOfInterns
+    println("Общее количество сотрудников $totalAmountEmployees человек")
+    val averageSalaryEmployees: Int = totalExpsForSalary / totalAmountEmployees
+    println("Средняя ЗП одного сотрудника после устройства стажеров $averageSalaryEmployees рублей")
 }
