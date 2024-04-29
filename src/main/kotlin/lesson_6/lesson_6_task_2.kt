99ce6c471ba37dcb00d64e71e00ceb58d64554fd
Maxim <55555lab@mail.ru>
2024-04-29 19:38:14 +0300

    KS-6-2 fix

--- src/main/kotlin/lesson_6/lesson_6_task_2.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_2.kt
@@ -9,26 +9,9 @@ fun main() {
      “Прошло N секунд”. Где N – количество  секунд, введенных пользователем. Программа должна вставать на паузу.
      */
     println(
-        "Это простой таймер, который засекает не более 60-ти секунд.\n" +
-            "Введите количество секунд:",
+        "Это простой таймер! Введите количество секунд:"
     )
     val numberOfSeconds = readln().toLong()
-    var second = "секунд"
-    var passed = "Прошло"
-    val listNumbersSecondOne = listOf<Long>(2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54)
-    val listNumbersSecondTwo = listOf<Long>(1, 21, 31, 41, 51)
-
-    while (true) {
-        println("Запущен таймер на указанное Вами время")
-        Thread.sleep(numberOfSeconds * NUMBER_OF_MILLISECONDS_IN_SECOND)
-        if (numberOfSeconds in listNumbersSecondOne) {
-            second = "секунды"
-        } else if (numberOfSeconds in listNumbersSecondTwo) {
-            passed = "Прошла"
-            second = "секунда"
-        }
-        println("$passed $numberOfSeconds $second")
-        break
-    }
+    Thread.sleep(numberOfSeconds * NUMBER_OF_MILLISECONDS_IN_SECOND)
+    println("Прошло $numberOfSeconds секунд")
 }
-// Вань я знаю что инициатива наказуема! Я знаю что нужно следовать ТЗ, но ПМ был бы рад таким правкам))))
