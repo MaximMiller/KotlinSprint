944cf69100a13d2c3b16c4fcec7fe3a705d065ee
Maxim <55555lab@mail.ru>
2024-05-03 14:51:31 +0300

    KS-7-5 fix

--- src/main/kotlin/lesson_7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -4,26 +4,33 @@ const val MIN_LENGTH_PASSWORD = 6
 
 fun main() {/*
     Создай программу, которая генерирует более сложные пароли.
-– пароль должен содержать цифры, строчные и заглавные буквы - должны присутствовать все три вида символов;
+– пароль должен содержать цифры, строчные и ЗАГЛАВНЫЕ БУКВЫ - должны присутствовать все три вида символов;
 – цифры и буквы должны располагаться в случайном порядке;
 – длина пароля должна задаваться пользователем;
 – минимальная длина пароля 6 символов.
      */
     println("Генерируем сложный пароль - укажите длину:")
-    var lengthPassword = readln().toInt()
+    val lengthPassword = readln().toInt()
 
     val listChar = ('0'..'9') + ('a'..'z') + ('A'..'Z')
+    val intRange = '0'..'9'
+    val charRange = 'a'..'z'
+    val capitalLetters = 'A'..'Z'
     var complexPasswords = ""
 
-    while (true) {
-        if (lengthPassword < MIN_LENGTH_PASSWORD) {
-            println("Символов в пароле должно быть не меньше $MIN_LENGTH_PASSWORD")
-            lengthPassword = readln().toInt()
-        } else {
-            for (i in 1..lengthPassword) {
-                complexPasswords += listChar.random()
-            }
-            break
+    if (lengthPassword < MIN_LENGTH_PASSWORD) {
+        for (i in 1..MIN_LENGTH_PASSWORD) {
+            complexPasswords += intRange.random()
+            complexPasswords += charRange.random()
+            complexPasswords += capitalLetters.random()
+            complexPasswords += listChar.random()
+        }
+    } else {
+        for (i in 1..lengthPassword) {
+            complexPasswords += listChar.random()
+            complexPasswords += intRange.random()
+            complexPasswords += charRange.random()
+            complexPasswords += capitalLetters.random()
         }
     }
     println(complexPasswords)
