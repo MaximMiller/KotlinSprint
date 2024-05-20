cc56d13b0c94a0b4681fe621ed1d6aa9038dec2c
Maxim <55555lab@mail.ru>
2024-05-20 15:01:07 +0300

    KS-13-3 complete

---
+++ src/main/kotlin/lesson_13/TelephoneDir.kt
@@ -0,0 +1,33 @@
+package org.example.lesson_13
+
+class TelephoneDir(
+    val userName: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+) {
+    fun printData() {
+        println("Имя: $userName, Номер: $phoneNumber, Компания:$company")
+    }
+}
+
+fun main() {
+    val listContact = listOf(
+        TelephoneDir("Max", 8953147569955),
+        TelephoneDir("Fill", 8953147569955, "VK"),
+        TelephoneDir("Bob", 8953147569955, "Sber"),
+        TelephoneDir("Anna", 8953147569955),
+        TelephoneDir("Li", 8953147569955)
+    )
+    for (i in listContact) i.printData()
+
+}
+/*
+Скопируй класс из предыдущей задачи и измени его название для избегания конфликтов.
+
+Для того, чтобы взаимодействовать со всеми записями телефонной книги, как с одним целым, их нужно объединить в список.
+
+- создай и инициализируй (одновременно) список с 5 объектами класса-контакта;
+- 2 контакта должны принимать null для поля компании;
+- 1 контакт должен иметь название компании “null”;
+- выведи список всех существующих компаний, которые есть в телефонной книге.
+ */
