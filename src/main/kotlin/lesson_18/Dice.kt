ab1099c77b6b050a138f8ca52db5616ff985219f
Maxim <55555lab@mail.ru>
2024-06-05 12:29:02 +0300

    KS-18-2 fix

--- src/main/kotlin/lesson_18/Dice.kt
+++ src/main/kotlin/lesson_18/Dice.kt
@@ -1,32 +1,59 @@
 package org.example.lesson_18
 
-open class Dice(
-    private val countFacet: Int,
-) {
-    private var valueDice = 0
-    fun throwDice(): Int {
-        val oneDice = (1..countFacet).random()
-        val twoDice = (1..countFacet).random()
-        valueDice = oneDice + twoDice
-        return valueDice
+abstract class Dice {
+    abstract fun throwDice(): Int
+
+    abstract fun printValueDice()
+}
+
+class DiceFourFacet : Dice() {
+    private val countFacet = 4
+    private var oneDice = 0
+
+    override fun throwDice(): Int {
+        oneDice = (1..countFacet).random()
+        return oneDice
     }
 
-    fun printValueDice() {
-        println("Общее значение костей $valueDice")
+    override fun printValueDice() {
+        println("Значение кости $oneDice")
+    }
+
+
+}
+
+
+class DiceSixFacet : Dice() {
+    private val countFacet = 6
+    private var oneDice = 0
+
+    override fun throwDice(): Int {
+        oneDice = (1..countFacet).random()
+        return oneDice
+    }
+
+    override fun printValueDice() {
+        println("Значение кости $oneDice")
     }
 }
 
-class DiceFourFacet(countFacet: Int) : Dice(countFacet)
+class DiceEightFacet : Dice() {
+    private val countFacet = 8
+    private var oneDice = 0
+    override fun throwDice(): Int {
+        oneDice = (1..countFacet).random()
+        return oneDice
+    }
 
-class DiceSixFacet(countFacet: Int) : Dice(countFacet)
-
-class DiceEightFacet(countFacet: Int) : Dice(countFacet)
-
+    override fun printValueDice() {
+        println("Значение кости $oneDice")
+    }
+}
 
 fun main() {
-    val diceFourFacet = DiceFourFacet(4)
-    val diceSixFacet = DiceSixFacet(6)
-    val diceEightFacet = DiceEightFacet(8)
+    val diceFourFacet = DiceFourFacet()
+    val diceSixFacet = DiceSixFacet()
+    val diceEightFacet = DiceEightFacet()
 
     val listDice = listOf<Dice>(diceFourFacet, diceSixFacet, diceEightFacet)
     listDice.forEach { it.throwDice() }.also { listDice.forEach { it.printValueDice() } }
