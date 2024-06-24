8031c4ea35b2286899d49c201f40581401ae3a82
Maxim <55555lab@mail.ru>
2024-06-24 08:51:45 +0300

    KS-21-1 complete

---
+++ src/main/kotlin/lesson_21/NumberOfVowels.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_21
+
+fun String.vowelCount(): Int {
+    val listVowel = arrayOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+    return this.count { it in listVowel }
+}
+
+fun main() {
+    val str = "hello"
+    println(str.vowelCount())
+}
+/*
+Реализуй extension функцию для класса String, которая будет
+возвращать количество гласных букв в строке. Назови функцию vowelCount.
+Например, для строки "hello" функция должна вернуть 2.
+ */
